5b41645c12bb9a683ba04f608c7b8ba212bf5991
fajari <m.soleh.fajari@gmail.com>
2022-11-19 22:13:55 +0700

    Update add eartquake_alert

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ Application to detect current Earth Quake
 This Application will scrape data from earthquake.usgs.gov and bmkg.go.id
 Maintain by. MSF
 """
-import IndonesiaLatestEarthQuakeAlert
+import earthquake_alert
 
 if __name__ == '__main__':
-    result = IndonesiaLatestEarthQuakeAlert.data_extraction()
-    IndonesiaLatestEarthQuakeAlert.show_data(result)
+    result = earthquake_alert.data_extraction()
+    earthquake_alert.show_data(result)
