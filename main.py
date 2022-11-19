0326a4d3ea455620150bf2fb9aea669e3b221217
fajari <m.soleh.fajari@gmail.com>
2022-11-19 22:07:24 +0700

    Update remove comment

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ Application to detect current Earth Quake
 This Application will scrape data from earthquake.usgs.gov and bmkg.go.id
 Maintain by. MSF
 """
-# from earthquake_alert import data_extraction, show_data
 import IndonesiaLatestEarthQuakeAlert
 
 if __name__ == '__main__':
