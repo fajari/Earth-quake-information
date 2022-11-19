38072239b0398a93cd1e33c17243eb4a4ed51f5d
fajari <m.soleh.fajari@gmail.com>
2022-11-19 21:53:01 +0700

    Update import

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ This Application will scrape data from earthquake.usgs.gov and bmkg.go.id
 Maintain by. MSF
 """
 # from earthquake_alert import data_extraction, show_data
-import earthquake_alert
+import IndonesiaLatestEarthQuakeAlert
 
 if __name__ == '__main__':
-    result = earthquake_alert.data_extraction()
-    earthquake_alert.show_data(result)
+    result = data_extraction()
+    show_data(result)
